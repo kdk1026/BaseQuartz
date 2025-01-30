94bd06f071a63ae6c2377c9ccd21583e6b538104
kdk1026 <kdk1026@gmail.com>
2025-01-30 20:49:04 +0900

    데이터소스, 마이바비트 설정 분리

--- src/main/java/com/kdk/cofnig/app/DataSourceConfig.java
+++ src/main/java/com/kdk/cofnig/app/DataSourceConfig.java
@@ -2,17 +2,9 @@ package com.kdk.cofnig.app;
 
 import javax.sql.DataSource;
 
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.mybatis.spring.SqlSessionFactoryBean;
-import org.mybatis.spring.SqlSessionTemplate;
-import org.mybatis.spring.annotation.MapperScan;
-import org.mybatis.spring.boot.autoconfigure.SpringBootVFS;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -33,11 +25,7 @@ import com.zaxxer.hikari.HikariDataSource;
  */
 @Configuration
 @EnableTransactionManagement
-@MapperScan(basePackages = {"com.kdk.app.**.mapper"}, annotationClass = Mapper.class)
 public class DataSourceConfig {
-
-	private static final String MYBATIS_CONFIG_LOCATION = "mybatis/configuration.xml";
-	private static final String MYBATIS_MAPPER_LOCATION = "classpath:mybatis/mappers/**/*.xml";
 
 	@Bean
     @ConfigurationProperties(prefix = "spring.datasource.hikari")
@@ -53,21 +41,6 @@ public class DataSourceConfig {
     @Bean
     PlatformTransactionManager transactionManager(DataSource dataSource) {
 		return new DataSourceTransactionManager(dataSource);
-	}
-
-    @Bean
-    SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
-    	SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
-		sqlSessionFactory.setDataSource(dataSource);
-		sqlSessionFactory.setConfigLocation(new ClassPathResource(MYBATIS_CONFIG_LOCATION));
-		sqlSessionFactory.setMapperLocations(new PathMatchingResourcePatternResolver().getResources(MYBATIS_MAPPER_LOCATION));
-		sqlSessionFactory.setVfs(SpringBootVFS.class);
-		return sqlSessionFactory.getObject();
-	}
-
-    @Bean
-    SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
-		return new SqlSessionTemplate(sqlSessionFactory);
 	}
 
 }
